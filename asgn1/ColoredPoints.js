3eacb6884c5cd7c07797dee2e75ac7694577c4ed
Nathan Leroy <nathanleroy@Nathans-MacBook-Air.local>
2026-01-19 19:24:27 -0800

    fix size slider

--- asgn1/ColoredPoints.js
+++ asgn1/ColoredPoints.js
@@ -75,7 +75,7 @@ function addActionsForHtmlUI(){
     document.getElementById('greenSlide').addEventListener('mouseup', function() { g_selectedColor[1] = this.value/100; });
     document.getElementById('blueSlide').addEventListener('mouseup', function() { g_selectedColor[2] = this.value/100; });
 
-    document.getElementById('blueSlide').addEventListener('mouseup', function() { g_selectedSize = this.value; });
+    document.getElementById('sizeSlide').addEventListener('mouseup', function() { g_selectedSize = this.value; });
 }
 
 function main() {
